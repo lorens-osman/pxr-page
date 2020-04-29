6df3bdbc9ea9e23dcb129c21298234f82f93dd07
lorens-osman <lorans.ev@gmail.com>
2020-04-29 07:17:22 +0300

    blue theme styling

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
   
   
   var typedo = new Typed('.element', {
-    strings: ["px <i class='fa fa-angle-right'></i> rem <i class='fa fa-check-circle-o'></i>", "Direction sensitive <i class='fa fa-check-circle-o'></i>"],
+    strings: ["px <i class='fa fa-angle-right'></i> rem <i class='fa fa-check-circle-o'></i>", "Direction sensitive <i class='fa fa-check-circle-o' style='color:green;'></i>"],
     typeSpeed: 90,
     showCursor: true,
     loop:true
